1d1b9cbf6366a78b86f37e0a8365bddfa12a1abe
Sifon Isaac <syfonisaac@gmail.com>
2021-07-23 10:41:12 +0100

    new chnages

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,22 @@
 window.addEventListener('load', () => {
   aos_init();
 });
+
+
+/*
+    Carousel
+*/
+let items = document.querySelectorAll('.carousel .cus')
+items.forEach((el) => {
+  const minPerSlide = 3
+  let next = el.nextElementSibling
+  for (var i = 1; i < minPerSlide; i++) {
+      if (!next) {
+          // wrap carousel by using first child
+          next = items[0]
+      }
+      let cloneChild = next.cloneNode(true)
+      el.appendChild(cloneChild.children[0])
+      next = next.nextElementSibling
+  }
+})
